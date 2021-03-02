9e568e215e3bd61adc071e573a9947657277699b
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-03-02 01:24:41 -0300

    adicionando modal ao clicar

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ const cs = (el)=> document.querySelectorAll(el);  /// para simplificar todos os 
 pizzaJson.map((item, index) => {
   let pizzaItem = c('.models .pizza-item').cloneNode(true);  /// esse cloneNode(true) pega o item e tudo o que esá dentro dele, no caso, pega o pizza-item e tudo o que está dentro dele;
   
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;  /// toFixed(2) para ter duas casas após a virgula;
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-  pizzaItem.querySelector('.pizza-item--price').innerHTML = item.price;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  pizzaItem.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    c('.pizzaWindowArea').style.opacity = 0;
+    c('.pizzaWindowArea').style.display = 'flex';
+    setTimeout(() => {
+      c('.pizzaWindowArea').style.opacity = 1;
+    }, 200)
+
+  });
+
 
   c('.pizza-area').append( pizzaItem );  /// esse .append é para adicionar mais um item igual, pois se colocar somente o .innerHTML, ele irá substituir cada vez;
 })
+
+/// parei na parte 4;   https://alunos.b7web.com.br/curso/javascript/compra-de-pizzas-parte-5
